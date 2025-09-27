300d0c65f6ec03e03f2d3a3738d7fc87785c00f0
zachar7 <springerzman@gmail.com>
2025-09-27 16:05:23 -0700

    asd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/lordbaldwin1.jpg",
+        url: "/og/home",
         width: 1200,
         height: 630,
         alt: "Zachary Springer - Developer, gamer, and gym guy.",
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     title: "Zachary Springer",
     card: "summary_large_image",
     creator: "@lordbaldwin1",
-    images: ["/lordbaldwin1.jpg"],
+    images: ["/og/home"],
   },
 }
 
